77888ce8781d8ba0ccd41d03ead31485d6c1a05c
Oleg Skarednov <olegskar1@gmail.com>
2023-01-30 20:16:22 +0500

    feat: base routes,suspense, chunks webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import { Counter } from './components/Counter';
+import { Suspense } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
 const App = () => {
   return (
     <div className="app">
-      <Counter />
+      <Link to="/">Main</Link>
+      <Link to="/about">About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.scss';
 
-render(<App />, document.getElementById('root'));
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
