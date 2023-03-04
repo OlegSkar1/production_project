b8d844eda2ccd25c3128308f17b8a2b779e29f41
Oleg Skarednov <olegskar1@gmail.com>
2023-03-04 16:29:48 +0500

    fix test

--- src/shared/lib/tests/TestAsyncThunk/TestAsyncThunk.ts
+++ src/shared/lib/tests/TestAsyncThunk/TestAsyncThunk.ts
@@ -1,25 +1,34 @@
 import { AsyncThunkAction } from '@reduxjs/toolkit';
 
+import axios, { AxiosStatic } from 'axios';
+
 import { StateSchema } from 'app/providers/StoreProvider';
 
 type ActionCreatorType<Returned, Arg, RejectedValue> = (
   arg: Arg
 ) => AsyncThunkAction<Returned, Arg, { rejectValue: RejectedValue }>;
 
+jest.mock('axios');
+
+const mockedAxios = jest.mocked(axios, true);
+
 export class TestAsyncThunk<Returned, Arg, RejectedValue> {
   dispatch: jest.MockedFn<any>;
   getState: () => StateSchema;
   actionCreator: ActionCreatorType<Returned, Arg, RejectedValue>;
+  api: jest.MockedFunctionDeep<AxiosStatic>;
 
   constructor(actionCreator: ActionCreatorType<Returned, Arg, RejectedValue>) {
     this.actionCreator = actionCreator;
     this.dispatch = jest.fn();
     this.getState = jest.fn();
+
+    this.api = mockedAxios;
   }
 
   async callThunk(arg: Arg) {
     const action = this.actionCreator(arg);
-    const result = await action(this.dispatch, this.getState, undefined);
+    const result = await action(this.dispatch, this.getState, { api: this.api });
 
     return result;
   }
