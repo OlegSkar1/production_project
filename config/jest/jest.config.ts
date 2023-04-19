2ac9e340e56e8fd18288d9ed66221c97e7b339fc
Oleg Skarednov <olegskar1@gmail.com>
2023-04-19 11:32:53 +0500

    feat html jest report

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -31,6 +31,18 @@ export default {
     __API__: '',
     __PROJECT__: 'jest',
   },
+  reporters: [
+    'default',
+    [
+      'jest-html-reporters',
+      {
+        publicPath: '<rootDir>/reporters/jest',
+        filename: 'report.html',
+        openReport: true,
+        inlineSource: true,
+      },
+    ],
+  ],
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
 
