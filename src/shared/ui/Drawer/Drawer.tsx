14240372504acd8b342fa6bdbee1f8d9e5a3c0f6
Oleg Skarednov <olegskar1@gmail.com>
2023-04-27 01:27:37 +0500

    feat: Drawer drag

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,8 +1,9 @@
-import { ReactNode } from 'react';
+import { a, config, useSpring } from '@react-spring/web';
+import { useDrag } from '@use-gesture/react';
+import { ReactNode, useCallback, useEffect } from 'react';
 
 import cls from './Drawer.module.scss';
 
-import { useModal } from '../../lib/hooks/useModal';
 import { Overlay } from '../Overlay/Overlay';
 import { Portal } from '../Portal/Portal';
 
@@ -17,32 +18,65 @@ interface DrawerProps {
   lazy?: boolean;
 }
 
+const height = window.innerHeight - 100;
+
 export const Drawer: React.FC<DrawerProps> = (props) => {
-  const { className, children, isOpen, onClose, lazy } = props;
+  const { className, children, isOpen, onClose } = props;
 
   const { theme } = useTheme();
 
-  const { isClosing, isMounted, onCloseHandler } = useModal({ isOpen, onClose });
+  const [{ y }, api] = useSpring(() => ({ y: height }));
 
-  const mods: Record<string, boolean | undefined> = {
-    [cls.opened]: isOpen,
-    [cls.closed]: isClosing,
+  const openDrawer = useCallback(() => {
+    api.start({
+      y: 0,
+      immediate: false,
+    });
+  }, [api]);
+
+  useEffect(() => {
+    if (isOpen) {
+      openDrawer();
+    }
+  }, [api, isOpen, openDrawer]);
+
+  const close = (velocity = 0) => {
+    api.start({
+      y: height,
+      config: { ...config.stiff, velocity },
+      onResolve: onClose,
+    });
   };
 
-  if (!isMounted && lazy) {
+  const bind = useDrag(
+    ({ last, velocity: [, vy], direction: [, dy], movement: [, my], cancel }) => {
+      if (my < -70) cancel();
+
+      if (last) {
+        my > height * 0.5 || (vy > 0.5 && dy > 0) ? close(vy) : openDrawer();
+      } else api.start({ y: my, immediate: true });
+    },
+    {
+      from: () => [0, y.get()],
+      filterTaps: true,
+      bounds: { top: 0 },
+      rubberband: true,
+    }
+  );
+
+  if (!isOpen) {
     return null;
   }
 
+  const display = y.to((py) => (py < height ? 'block' : 'none'));
+
   return (
     <Portal>
-      <div
-        className={classNames(cls.drawer, [className, theme, 'app_drawer'], mods)}
-        onAnimationEnd={isClosing ? onClose : undefined}
-      >
-        <Overlay onClick={onCloseHandler} />
-        <div className={cls.contentWrapper}>
-          <div className={cls.content}>{children}</div>
-        </div>
+      <div className={classNames(cls.drawer, [className, theme, 'app_drawer'], {})}>
+        <Overlay onClick={close} />
+        <a.div className={cls.sheet} style={{ display, bottom: `calc(-100vh + ${height - 100}px)`, y }} {...bind()}>
+          {children}
+        </a.div>
       </div>
     </Portal>
   );
