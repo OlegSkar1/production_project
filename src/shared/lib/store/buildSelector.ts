d273cb3e93bb59993c05dc98ee23790a65c515f4
Oleg Skarednov <olegskar1@gmail.com>
2023-05-15 21:16:02 +0500

    fix: buildSelector

--- src/shared/lib/store/buildSelector.ts
+++ src/shared/lib/store/buildSelector.ts
@@ -2,12 +2,13 @@ import { useSelector } from 'react-redux';
 
 import { StateSchema } from '@/app/providers/StoreProvider';
 
-type Selector<T> = (state: StateSchema) => T;
-type Result<T> = [() => T, Selector<T>];
+type Selector<T, Args extends unknown[]> = (state: StateSchema, ...args: Args) => T;
+type Hook<T, Args extends unknown[]> = (...args: Args) => T;
+type Result<T, Args extends unknown[]> = [Hook<T, Args>, Selector<T, Args>];
 
-export function buildSelector<T>(selector: Selector<T>): Result<T> {
-  const useSelectorHook = () => {
-    return useSelector(selector);
+export function buildSelector<T, Args extends unknown[]>(selector: Selector<T, Args>): Result<T, Args> {
+  const useSelectorHook: Hook<T, Args> = (...args) => {
+    return useSelector((state: StateSchema) => selector(state, ...args));
   };
 
   return [useSelectorHook, selector];
